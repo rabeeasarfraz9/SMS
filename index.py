ec4dc6107411f17648be0297ed804cdee151f94e
Rabeea <rabeeasarfraz9@gmail.com>
2025-07-12 05:10:37 -0700

    Add_student

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
 students=[]
+
+def add_student():
+    name= input("Enter Student's Name:\n")
+    roll_no= input("Enter Roll_No:\n")
+    course= input("Enter Course:\n")
+    student= {'name': name, 'roll_no': roll_no, 'course': course}
+    students.append(student)
+    print("Added Student Successfully\n")
